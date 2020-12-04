0711a419977d87aed29a8d6f59d2f8ae1db5b05a
Crowdbotics <team@crowdbotics.com>
2020-12-04 09:12:55 +0000

    Update models

--- backend/manage.py
+++ backend/manage.py
@@ -5,7 +5,9 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'msm_mobile_041298_d_16278.settings')
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "msm_mobile_041298_d_16278.settings"
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,5 +19,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
